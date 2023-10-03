8abefebb77fc3013fec95e196884d59ba30eaebb
alexjruc <justworks2021@gmail.com>
2023-10-02 20:04:49 -0500

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import WeatherAppHeader from "./components/WeatherAppHeader";
 
 function App() {
     const [weather, setWeather] = useState(null);
+    const [weatherCityFound, setWeatherCityFound] = useState(null)
+
 
     const success = (position) => {
         const lat = position.coords.latitude;
@@ -21,6 +23,19 @@ function App() {
             .catch((err) => console.log(err));
     };
 
+    const findCity = (e) => {
+        const API_KEY = "ac2edf6ac368a4221f19d4386732de5c";
+        if(e.key === 'Enter' || e.keyCode === 13) {
+            let city = document.querySelector("input").value
+            
+            axios
+                .get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
+                    .then(({ data }) => setWeatherCityFound((data)))
+                    .catch((err) => console.log(err));
+                
+                    console.log(weatherCityFound);
+        }
+    }
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(success);
     }, []);
@@ -67,6 +82,7 @@ function App() {
             ) : (
                 <>
                 <WeatherAppHeader />
+                <input onKeyUp={findCity} className="bg-[#52B5E8] shadow-[0px_4px_4px_0px_rgba(255,_255,_255,_0.25)_inset] w-60 h-12 rounded-lg placeholder-[#D5F3FF] placeholder:bg-[url('/lupa.png')] placeholder:bg-no-repeat outline-none  px-2 dark:bg-[#201F3C] dark:shadow-[0px_4.8546px_4.8546px_0px_rgba(0,_0,_0,_0.25)_inset] dark:placeholder-[#76749E] " type="text" placeholder="        Busca una ciudad" />
                 <WeatherContainer weather={weather} />
                 </>
             )}
--- src/components/WeatherAppHeader.jsx
+++ src/components/WeatherAppHeader.jsx
@@ -1,26 +1,10 @@
-import axios from "axios";
 import { useState } from "react";
 
-const WeatherAppHeader = ({weather}) => {
+const WeatherAppHeader = () => {
 
-    const [weatherCityFound, setWeatherCityFound] = useState(null)
     const [isDark, setIsDark] = useState(false)
     
-    const findCity = (e) => {
-        const API_KEY = "ac2edf6ac368a4221f19d4386732de5c";
-        if(e.keyCode === 13) {
-            let city = document.querySelector("input").value
-            
-            axios
-                .get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
-                    .then(({ data }) => setWeatherCityFound((data)))
-                    .catch((err) => console.log(err));
-                
-                weather = weatherCityFound
-        }
-        //return weather
-        console.log(weather);
-    }
+    
     
     const handleDarkMode = () => {
         setIsDark(!isDark);
@@ -39,13 +23,12 @@ const WeatherAppHeader = ({weather}) => {
     }
 
     return (
-        <div className="w-screen grid grid-cols-2 grid-rows-2 px-4 text-white gap-4 md:flex md:justify-between md:p-4 md:max-w-5xl">
+        <div className="w-screen grid grid-cols-2 grid-rows-2 p-4 text-xl text-white gap-4 md:flex md:justify-between md:p-4 md:max-w-5xl">
             <h3 className="md:text-2xl">Weather app</h3>
             <div className="justify-self-end">
                 <i onClick={handleDarkMode} className='bx bxs-toggle-right hidden text-[#53388F] dark:text-[#52B5E8] cursor-pointer text-4xl md:text-5xl' ></i>
                 <i onClick={handleDarkMode} className='bx bxs-toggle-left text-[#53388F] dark:text-[#52B5E8] cursor-pointer text-4xl justify-self-end md:text-5xl'></i>
             </div>
-            <input onKeyUp={findCity} className="bg-[#52B5E8] shadow-[0px_4px_4px_0px_rgba(255,_255,_255,_0.25)_inset] col-span-2 rounded-lg placeholder-[#D5F3FF] placeholder:bg-[url('/lupa.png')] placeholder:bg-no-repeat outline-none  px-2 dark:bg-[#201F3C] dark:shadow-[0px_4.8546px_4.8546px_0px_rgba(0,_0,_0,_0.25)_inset] dark:placeholder-[#76749E] " type="text" placeholder="        Busca una ciudad" />
         </div>
     )
 }
